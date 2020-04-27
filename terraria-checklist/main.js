44e1c32a8f04bc88d59b2d85b8e18ef8ed38235a
ewauq <emilien.wauquier@gmail.com>
2020-04-27 21:46:26 +0200

    added terraria checklist (wip)

---
+++ terraria-checklist/main.js
@@ -0,0 +1,60 @@
+const checkboxButtons = document.querySelectorAll('.checkbox');
+const resetButton = document.getElementsByName('reset')[0];
+
+// Checkbox click handler
+for (const checkboxButton of checkboxButtons) {
+  checkboxButton.addEventListener('click', (event) => {
+
+    const target = event.target;
+    const parent = event.target.parentElement.parentNode;
+
+    const name = target.attributes.name.nodeValue;
+
+    if (target.classList.contains('checked')) {
+      target.classList.remove('checked')
+      parent.classList.remove('completed')
+      localStorage.setItem(name, false);
+    }
+    else {
+      target.classList.add('checked')
+      parent.classList.add('completed')
+      localStorage.setItem(name, true);
+    }
+  });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.length > 0) {
+
+    const keys = { ...localStorage }
+
+    for (const name in keys) {
+      const value = localStorage.getItem(name) == 'true'; // conversion to bool
+
+      if (document.getElementsByName(name).length) {
+
+        const target = document.getElementsByName(name)[0];
+        const parent = target.parentElement.parentNode;
+
+        if (value) {
+          target.classList.add('checked')
+          parent.classList.add('completed')
+        }
+
+      }
+
+    }
+
+  }
+});
+
+// Reset button handling
+resetButton.addEventListener('click', () => {
+  if (confirm('Reset the checklist progression?')) {
+    localStorage.clear();
+    location.reload();
+  }
+})
+
+
+console.debug("main.js loaded.");
