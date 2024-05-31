e36d557661c0764f80248c26c3535dba00e398e0
ewauq <emilien.wauquier@gmail.com>
2024-05-31 13:36:45 +0200

    Migrate the Terraria Checklist to the new build

---
+++ terraria-checklist/v1/main.js
@@ -0,0 +1,97 @@
+const checkboxButtons = document.querySelectorAll('input[type=checkbox]');
+const resetButton = document.getElementsByName('reset')[0];
+const progressCompleted = document.querySelector('.progress-completed');
+const progressUncompleted = document.querySelector('.progress-uncompleted');
+
+// Checkbox click handler
+for (const checkboxButton of checkboxButtons) {
+  checkboxButton.addEventListener('click', (event) => {
+
+    const target = event.target;
+    const parent = event.target.parentElement.parentNode;
+    const name = target.attributes.name.nodeValue;
+
+    if (target.checked) {
+      parent.classList.add('completed');
+      localStorage.setItem(name, true);
+    }
+    else {
+      parent.classList.remove('completed');
+      localStorage.setItem(name, false);
+    }
+
+    refreshProgessbar();
+  });
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+
+  // Loading the progress
+  if (localStorage.length > 0) {
+
+    const keys = Object.keys(localStorage);
+
+    for (const name of keys) {
+      const value = localStorage.getItem(name) == 'true'; // conversion to bool
+
+      if (document.getElementsByName(name).length) {
+
+        const target = document.getElementsByName(name)[0];
+        const parent = target.parentElement.parentNode;
+
+        if (value) {
+          target.setAttribute('checked', 'checked');
+          parent.classList.add('completed');
+        }
+
+      }
+
+    }
+
+  }
+
+  // Progress bar stuff
+  refreshProgessbar();
+  progressCompleted.classList.add('animated');
+
+});
+
+// Reset button handling
+resetButton.addEventListener('click', () => {
+  if (confirm('This will reset the checklist progression. Are you sure?')) {
+    localStorage.clear();
+    location.reload();
+  }
+})
+
+// On window resizing
+window.addEventListener("resize", refreshProgessbar);
+
+// Refreshes the progress bar at the bottom of the window
+function refreshProgessbar() {
+  const checkboxesTotal = checkboxButtons.length;
+  const checkedCheckboxes = document.querySelectorAll('input[type=checkbox]:checked').length;
+  const percent = Math.round(checkedCheckboxes * 100 / checkboxesTotal);
+  const progessLabel = `${checkedCheckboxes}/${checkboxesTotal} (${percent}%)`;
+
+  progressCompleted.style.width = `${percent}%`;
+
+  if (progressCompleted.offsetWidth < 110) {
+    progressUncompleted.innerHTML = progessLabel;
+    progressCompleted.innerHTML = '';
+  } else {
+    progressUncompleted.innerHTML = '';
+    progressCompleted.innerHTML = progessLabel;
+  }
+
+  if (percent == 100) {
+    progressUncompleted.style.display = "none";
+    progressCompleted.classList.add('completed');
+  } else {
+    progressUncompleted.style.display = "inline-block";
+    progressCompleted.classList.remove('completed');
+  }
+}
+
+console.debug("main.js loaded.");
